65d210116f4569cafdcefd04cbb8f6bfdf38737c
TomSachot <74669910+TomSachot@users.noreply.github.com>
2021-04-25 15:49:30 +0200

    Update Scene2.js

--- Scene2.js
+++ Scene2.js
@@ -6,7 +6,28 @@ class SceneTwo extends Phaser.Scene{
     init(data){
     }
     preload(){
-        this.load.tilemapTiledJSON('map_2_placeholder', 'map_2_placeholder.json');
+        //this.load.images(map);
         this.load.image('joueur', 'assets/joueur.png'); 
+        this.load.image('powerup', 'assets/powerup.png');
 
+    }
+
+    create(){
+
+        //map
+
+        //Sprites
+        joueur = this.physics.add.sprite(position_x, position_y, 'joueur');
+        
+        coeur_1 = this.add.sprite(1230,50, 'coeur');
+        coeur_2 = this.add.sprite(1180,50, 'coeur');
+        coeur_3 = this.add.sprite(1130,50, 'coeur');
+        
+        demicoeur_1 = this.add.sprite(1230,50, 'demicoeur').setVisible(false);
+        demicoeur_2 = this.add.sprite(1180,50, 'demicoeur').setVisible(false);
+        demicoeur_3 = this.add.sprite(1130,50, 'demicoeur').setVisible(false);
+
+        argent = this.add.sprite(930, 50, 'argent').setScale(0.5);
+
+        power_up = this.add.sprite(50,360, 'powerup');
     }
