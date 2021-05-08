6d6a7e71fb5e8db28e8e40ac9cf596d15837779c
TomSachot <74669910+TomSachot@users.noreply.github.com>
2021-05-08 15:28:18 +0200

    Create test.tsx

---
+++ tiles/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="test" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+ <image source="../../Sidescroller 2D-Prog/GD1B_SIDE_SACHOT/Assets/neige.png" width="400" height="49"/>
+</tileset>
